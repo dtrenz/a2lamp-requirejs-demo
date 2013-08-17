9a7d32553df8d8323353a8ee37828d31a8a09140
Dan Trenz <dtrenz@gmail.com>
2013-08-17 10:47:35 -0400

    Adding requirejs usage example code

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
-$(function() {
+require.config({
+	paths: {
+		jquery: 'lib/jquery-1.9.1',
+		jqueryui: 'lib/jquery-ui-1.10.3.min'
+	},
+	shim: {
+		jqueryui: {
+			deps: [ 'jquery' ],
+			exports: '$'
+		}
+	}
+});
 
-    // $('body').css({
-    //     'background-color': config.bgColor,
-    //     'color': config.textColor
-    // });
+require([ 'jquery', 'lib/config', 'jqueryui' ], function( $, config ) {
 
-    // $('#accordion').accordion();
+	$(function() {
+
+	    $('body').css({
+	        'background-color': config.bgColor,
+	        'color': config.textColor
+	    });
+
+	    $('#accordion').accordion();
+
+	});
 
 });
--- js/lib/config.js
+++ js/lib/config.js
@@ -1,4 +1,4 @@
-var config = {
+define({
     bgColor: 'blue',
     textColor: 'green'
-};
+});
